0847afcf295c8736853c71ba1817add7b4240e11
Aromal A <82209054+aromal-a@users.noreply.github.com>
2024-06-05 01:04:22 +0530

    Update CompanySection.jsx

--- components/CompanySection.jsx
+++ components/CompanySection.jsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 export default function CompanySection() {
     return (
         <div className="py-4 bg-zinc-100">
-            <p className="py-4 text-lg font-medium text-center opacity-40">Trusted by the world’s best companies</p>
-            <div className="container flex flex-wrap items-center justify-between">
             </div>
         </div>
     );
