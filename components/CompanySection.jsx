252178c06f7fab216064d4e683bf224199f885bb
Aromal A <82209054+aromal-a@users.noreply.github.com>
2024-06-05 01:04:49 +0530

    Update CompanySection.jsx

--- components/CompanySection.jsx
+++ components/CompanySection.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function CompanySection() {
     return (
         <div className="py-4 bg-zinc-100">
-            </div>
+    
         </div>
     );
 }
