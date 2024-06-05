96d285bebcca4b523a689e16511f4a31cedc06de
Aromal A <82209054+aromal-a@users.noreply.github.com>
2024-06-05 23:19:51 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,8 +2,8 @@ export default function Contact() {
     return (
         <div>
             <h1>Contact Us</h1>
-            <p>Phone: 555-555-5555</p>
-            <p>Email:
+            <p>Phone:9207877261</p>
+            <p>Email:arconfordesigns@gmail.com
             </p>
         </div>
     );
