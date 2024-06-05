e63863c6e37de3fa3134ef68a5df32b6100b3e74
Aromal A <82209054+aromal-a@users.noreply.github.com>
2024-06-05 23:16:44 +0530

    Update ContactSection.jsx

--- components/ContactSection.jsx
+++ components/ContactSection.jsx
@@ -8,7 +8,6 @@ export default function ContactSection() {
                     <p className="text-2xl">Lets</p>
                     <h1 className="text-3xl font-medium lg:text-6xl">Get in touch</h1>
                 </div>
-                <a className="inline-flex items-center gap-1 px-6 py-3 text-sm text-white rounded-full shadow-lg bg-gray-950 hover:bg-gray-800 hover:ring-2 hover:ring-gray-950 ring-offset-2" href="">CONTACTUS <TbArrowUpRight className="text-xl" /> </a>
             </div>
         </div>
     )
