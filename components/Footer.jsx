3c5688ff4b86f625d702318d07162d23c47866aa
Aromal A <82209054+aromal-a@users.noreply.github.com>
2024-07-28 22:15:13 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,20 +11,10 @@ export default function Footer() {
                         <h2 className="pb-4 text-xl font-semibold">COMPANY</h2>
                         <div className="flex flex-col ">
                             <Link className="py-1 hover:underline" href="/about">About Us</Link>
-                            <Link className="py-1 hover:underline" href="/press">Press</Link>
-                            <Link className="py-1 hover:underline" href="/careers">Careers</Link>
                             <Link className="py-1 hover:underline" href="/contact">Contact</Link>
                         </div>
                     </div>
-                    <div>
-                        <h2 className="pb-4 text-xl font-semibold">DEVELOPMENT</h2>
-                        <div className="flex flex-col ">
-                            <Link className="py-1 hover:underline" href="/about">Documentation</Link>
-                            <Link className="py-1 hover:underline" href="/press">API Reference</Link>
-                            <Link className="py-1 hover:underline" href="/careers">Changelog</Link>
-                            <Link className="py-1 hover:underline" href="/contact">Status</Link>
-                        </div>
-                    </div>
+                   
                     <div>
                         <h2 className="pb-4 text-xl font-semibold">CONNECT</h2>
                         <div className="flex flex-col ">
